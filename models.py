36068ef845353f156669e70ae16bcf0e549b4f8d
C3real-kill3r <brybzi@gmail.com>
2018-07-10 23:38:39 +0300

    added requirements .txt

--- models.py
+++ models.py
@@ -12,9 +12,9 @@ def create_tables():
 		if results is not None:
 			pass
 		else:
-		    cursor.execute("CREATE TABLE `admin` (`id` int(11) NOT NULL,`username` varchar(100) NOT NULL,`password` varchar(100) NOT NULL) ENGINE=InnoDB DEFAULT CHARSET=latin1;")
+		    cursor.execute("CREATE TABLE `admin` (`id` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,`username` varchar(100) NOT NULL,`password` varchar(100) NOT NULL) ENGINE=InnoDB DEFAULT CHARSET=latin1;")
 		    cursor.execute("INSERT INTO `admin`(`AdminID`,`Username`,`Password`)VALUES (1, 'admin','112233');")
-		    cursor.execute("CREATE TABLE `comments` (`commentID` int(11) NOT NULL,`username` varchar(200) NOT NULL,`comment` text NOT NULL,`time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP) ENGINE=InnoDB DEFAULT CHARSET=latin1;")
-		    cursor.execute("CREATE TABLE `users` (`userID` int(11) NOT NULL,`name` varchar(100) NOT NULL,`username` varchar(100) NOT NULL,`email` varchar(100) NOT NULL,`password` varchar(100) NOT NULL,`registration_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP) ENGINE=InnoDB DEFAULT CHARSET=latin1;")
+		    cursor.execute("CREATE TABLE `comments` (`commentID` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,`username` varchar(200) NOT NULL,`comment` text NOT NULL,`time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP) ENGINE=InnoDB DEFAULT CHARSET=latin1;")
+		    cursor.execute("CREATE TABLE `users` (`userID` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,`name` varchar(100) NOT NULL,`username` varchar(100) NOT NULL,`email` varchar(100) NOT NULL,`password` varchar(100) NOT NULL,`registration_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP) ENGINE=InnoDB DEFAULT CHARSET=latin1;")
 	connection.commit()
 	connection.close()
